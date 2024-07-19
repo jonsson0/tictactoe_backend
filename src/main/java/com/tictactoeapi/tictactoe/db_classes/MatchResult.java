41e02991fa339ee52a57e98d24226db969f29a4f
Mads jonsson <madsjonsson1997@gmail.com>
2024-07-19 11:07:31 +0200

    Changed name from Symbol to winnerSymbol

--- src/main/java/com/tictactoeapi/tictactoe/db_classes/MatchResult.java
+++ src/main/java/com/tictactoeapi/tictactoe/db_classes/MatchResult.java
@@ -29,7 +29,7 @@ public class MatchResult {
     @Field("Winner")
     private String winner;
 
-    @Field("Symbol")
-    private String symbol;
+    @Field("WinnerSymbol")
+    private String winnerSymbol;
 
 }
